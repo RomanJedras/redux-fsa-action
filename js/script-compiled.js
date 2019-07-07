2eb043b1c09d8dea3e0eba7df58340d3107771dd
Roman Jędras <romanjedras@o2.pl>
2019-07-07 17:39:39 +0200

    Remove disptach from eaction

--- js/script-compiled.js
+++ js/script-compiled.js
@@ -1,49 +1,48 @@
 import uuid from 'uuid';
-import { dispatch } from 'redux';
 
 export const ADD_COMMENT = 'ADD_COMMENT';
 
 addComment = text => {
-	return dispatch({
+	return {
 		type: ADD_COMMENT,
 		text,
 		id: uuid.v4()
-	});
+	};
 };
 
 export const EDIT_COMMENT = 'EDIT_COMMENT';
 editComment = (text, id) => {
-	return dispatch({
+	return {
 		type: EDIT_COMMENT,
 		id,
 		text
-	});
+	};
 };
 
 export const REMOVE_COMMENT = 'REMOVE_COMMENT';
 removeComment = id => {
-	return dispatch({
+	return {
 		type: REMOVE_COMMENT,
 		id
-	});
+	};
 };
 
 export const THUMB_UP_COMMENT = 'THUMB_UP_COMMENT';
 
 thumbUpComment = (votes, id) => {
-	return dispatch({
+	return {
 		type: THUMB_UP_COMMENT,
 		id,
 		votes: vote++
-	});
+	};
 };
 
 export const THUMB_DOWN_COMMENT = 'THUMB_DOWN_COMMENT';
 
 thumbDownComment = (votes, id) => {
-	return dispatch({
+	return {
 		type: THUMB_DOWN_COMMENT,
 		id,
 		votes: vote--
-	});
+	};
 };
