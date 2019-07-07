ced0d8f4cb771632a07d5cdb8870d8180e73bcb3
Roman Jędras <romanjedras@o2.pl>
2019-07-07 17:21:43 +0200

    Add comments file to reducer & create combine reducer in reducer.js

--- reducer.js
+++ reducer.js
@@ -1,48 +1,11 @@
-import {ADD_COMMENT,REMOVE_COMMENT,EDIT_COMMENT,THUMB_DOWN_COMMENT, THUMB_UP_COMMENT} from './actions'
+import { combineReducers } from 'redux';
+import comments from './comments';
 
-const initialState = {
-	comments: [],
-	users: []
-};
 
-function reducer(state = initialState, action) {
-	switch(action.type) {
-		case ADD_COMMENT:
-			return Object.assign({}, state, {
-				comments: [
-					{
-						id: action.id,
-						text: action.text,
-						votes: 0
-					}
-					, ...state]
-			});
-		case EDIT_COMMENT :
-		return	 Object.assign({}, state, {
-			comments: state.comments.filter(comment => comment.id === action.id? ( [
-				{...comment, text: action.text}]):'')
-		});
-		
-		case THUMB_DOWN_COMMENT:
-			return   Object.assign({}, state, {
-				comments: state.comments.filter(comment => comment.id === action.id? ( [
-					{...comment, votes: comment.votes - 1
-					}]):'')
-			});
-		
-		case THUMB_UP_COMMENT:
-			return   Object.assign({}, state, {
-				comments: state.comments.filter(comment => comment.id === action.id? ( [
-					{...comment, votes: comment.votes + 1
-					}]):'')
-			});
-		
-		case REMOVE_COMMENT:
-			return Object.assign({}, state, {
-				comments: state.comments.filter(comment => comment.id !== action.id)
-			});
-		default:
-			return state;
-	}
-}
+const app = combineReducers({
+	comments,
+});
+
+
+
 
