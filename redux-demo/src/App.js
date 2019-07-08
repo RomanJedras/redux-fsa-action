3875081a01b996c044af1985d9ce6e7413d1621d
Roman Jędras <romanjedras@o2.pl>
2019-07-08 16:11:18 +0200

    Add monitor to chceck Action in component

--- redux-demo/src/App.js
+++ redux-demo/src/App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import './App.css';
 import CommentsList from './CommentsListContainer';
-
+import DevTools from './DevTools';
 
 const App = () => {
 	return (
 		<div className="App">
 		<h4>List of comments:</h4>
 		<CommentsList/>
+			<DevTools />
 		</div>
 );
 };
--- redux-demo/src/index.js
+++ redux-demo/src/index.js
@@ -4,16 +4,17 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore } from 'redux';
 import {addComment} from "./actions";
 import reducer from './reducer';
-import { createLogger } from 'redux-logger';
+//import { createLogger } from 'redux-logger';
+import DevTools from './DevTools';
 
-const logger = createLogger();
+//const logger = createLogger();
 
 const store = createStore(
 	reducer,
-	applyMiddleware(logger)
+	DevTools.instrument()
 );
 
 ReactDOM.render(<Provider store={store}>
